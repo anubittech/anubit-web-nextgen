41b7c2493d0b165d0a2327808f16a813725404ba
anubittech <anubit.dev@gmail.com>
2024-08-02 18:31:12 +0530

    c

--- src/components/section/FAQ.jsx
+++ src/components/section/FAQ.jsx
@@ -10,7 +10,7 @@ const FAQ = () => {
   return (
     <div className="redd flex min-h-96 w-screen flex-col items-center justify-center gap-10 p-5">
       <div className="redd flex h-fit w-full  justify-center">
-        <h1 className="text-2xl desktop:text-5xl py-5  font-bold bg-gradient-text uppercase">Frequently Asked Questions</h1>
+        <h1 className="text-xl desktop:text-5xl py-5  font-bold bg-gradient-text uppercase">Frequently Asked Questions</h1>
       </div>
       <div className="flex min-h-[50%] w-full flex-col items-center justify-center rounded-lg border px-5 text-2xl tablet:w-[35rem] laptop:w-[40rem] desktop:w-[75rem]">
         {FAQSheet.map((data, idx) => {
